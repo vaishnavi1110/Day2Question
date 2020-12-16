064ccd66a88f5b1761ddec9bc3ab40f11aff2585
vaishnavi gawande <Vaishnavi.Gawande@lntinfotech.com>
2020-12-16 16:37:06 +0530

    CS project

---
+++ SampleDay2QuestionSol/SampleDay2Question/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SampleDay2Question
+{
+    class Program
+    {
+        static void count_even_odd(int min, int max)
+        {
+
+
+            int n = 2;
+            for (int i = min; i < max; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            int min = 1, max = 7;
+
+
+            count_even_odd(min, max);
+
+        }
+    }
+}
+
+
